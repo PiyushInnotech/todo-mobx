cfa7eb90958519db52568edaa6a1adbfb48616ad
PiyushInnotech <piyushmangla@innotechteam.in>
2023-06-07 19:11:06 +0530

    todolist using mobx

--- src/store.js
+++ src/store.js
@@ -1,1 +1,25 @@
+import { makeObservable, observable, action } from "mobx";
 
+class TodoStore {
+  todos = ["n4fnjfnj"];
+
+  constructor() {
+    makeObservable(this, {
+      todos: observable,
+      addTodo: action,
+      removeTodo: action,
+    });
+  }
+
+  addTodo = (todo) => {
+    this.todos.push(todo);
+  };
+
+  removeTodo = (index) => {
+    this.todos.splice(index, 1);
+  };
+}
+
+const todoStore = new TodoStore();
+
+export { todoStore };
